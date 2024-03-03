a65098549813651a2ae12750b8024a96616ff3fd
anouar <anouar.ab95@gmail.com>
2024-03-03 18:10:03 +0100

    add model Event avec table et controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSedder::class,
         ]);
+        $this->call([
+            AdminSedder::class,
+        ]);
     }
 }
