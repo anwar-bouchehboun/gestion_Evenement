94908cc9477521e90c351a3d61f7c6ecb6bb6fbc
anouar <anouar.ab95@gmail.com>
2024-03-03 12:04:52 +0100

    update migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+  
     public function run(): void
     {
-       
+
         $this->call([
             CategorieSdeer::class,
         ]);
